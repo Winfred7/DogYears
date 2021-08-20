8d42fbd421527f828f5b82199fc5e55dc88a003e
winfred tornu <87378085+Winfred7@users.noreply.github.com>
2021-08-20 02:19:17 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+//my age
+const myAge=34;
+//early years
+let earlyYears=2;
+earlyYears *=10.5;
+
+//myAge decreased by 2
+let laterYears =myAge -2;
+
+//compute my later years to a dog year
+laterYears =laterYears*4;
+//my total age convert to a dog year
+let myAgeInDogYears=earlyYears + laterYears;
+//store my name in lowcase
+const myName='Winfred Tornu'.toLowerCase();
+
+//my human age and dog age
+console.log(`My name ${myName}.I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
